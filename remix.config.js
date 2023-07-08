68ca09e4dcee6e9766e1a2ea028a5c48c36d2b17
Eduardo Dallmann <eduardodallmann@gmail.com>
2023-07-08 14:53:34 -0300

    admin

--- remix.config.js
+++ remix.config.js
@@ -7,7 +7,7 @@ module.exports = {
   tailwind: true,
   serverBuildPath: 'build/index.js',
   serverModuleFormat: 'cjs',
-  serverDependenciesToBundle: [/^axios.*/, /^firebase.*/],
+  serverDependenciesToBundle: [/^axios.*/, /^firebase.*/, /^firebase-admin.*/],
   future: {
     v2_meta: true,
     v2_errorBoundary: true,
