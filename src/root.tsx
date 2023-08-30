dacd164de9551aec55bbf2dcb7f5af012e7c1013
Eduardo Dallmann <eduardodallmann@gmail.com>
2023-08-29 22:33:27 -0300

    remove jotai

--- src/root.tsx
+++ src/root.tsx
@@ -1,7 +1,4 @@
-import { useMemo } from 'react';
-
 import { useLoaderData } from '@remix-run/react';
-import { Provider, createStore } from 'jotai';
 
 import { Body } from './components/commons/body/body';
 import { LanguageProvider } from './global-context/language';
@@ -14,21 +11,16 @@ export { loader, links, meta } from './root.server';
 
 export default function App() {
   const { locale, themeMode } = useLoaderData<RootLoaderReturn>();
-  const store = useMemo(() => {
-    return createStore();
-  }, [themeMode, locale]);
 
   return (
-    <Provider store={store}>
-      <LanguageProvider {...locale}>
-        <ThemeProvider defaultTheme={themeMode}>
-          <SavingProvider>
-            <MenuProvider>
-              <Body />
-            </MenuProvider>
-          </SavingProvider>
-        </ThemeProvider>
-      </LanguageProvider>
-    </Provider>
+    <LanguageProvider {...locale}>
+      <ThemeProvider defaultTheme={themeMode}>
+        <SavingProvider>
+          <MenuProvider>
+            <Body />
+          </MenuProvider>
+        </SavingProvider>
+      </ThemeProvider>
+    </LanguageProvider>
   );
 }
